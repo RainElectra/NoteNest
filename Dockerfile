5cd969ca98802b417ab8e0f149f0d8479d214798
RainElectra <delured1@gmail.com>
2026-04-11 21:22:39 +0300

    docker fix

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,11 @@ RUN dotnet restore
 COPY . ./
 RUN dotnet publish -c Release -o out
 
-# runtime stage
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
-
 COPY --from=build /app/out .
 
-EXPOSE 8080
+ENV ASPNETCORE_URLS=http://+:10000
+EXPOSE 10000
 
 ENTRYPOINT ["dotnet", "MyApi.dll"]
--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826f327506301ca00a4fc2982b5611318c1dd9fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a8d2e643722a60a1cc66aea3217a9cc2627e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
